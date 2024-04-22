a79a11c34c2ffb34ffd8268790c787747b9af55f
Lee Xuan Ong <ongtan597@gmail.com>
2024-04-22 23:12:26 +0100

    json loads from websocket

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI , WebSocket, WebSocketDisconnect
 import signal
+import json
 
 app = FastAPI()
 
+cache_data = []
 
 @app.get("/")
 async def root():
@@ -13,7 +15,10 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
         data = await websocket.receive_text()
+        json_data = json.loads(data)
+        print(type(json_data))
         print(data)
+        
         await websocket.send_text(f"Data received: {data}")
     except WebSocketDisconnect:
         await websocket.close()
